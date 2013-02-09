05805b2244d880fb883f3e0e547703dac55d40d7
Dhanush Balachandran <dhanush@jiff.com>
2013-02-09 11:21:26 -0800

    Removing JFLog

--- Code/Support/RKLog.h
+++ Code/Support/RKLog.h
@@ -61,16 +61,16 @@
  than the level the message was logged at (in this case, Info).
  */
 #define RKLogCritical(...)                                                              \
-RKlcl_log(RKLogComponent, RKlcl_vCritical, @"" __VA_ARGS__);JFLog(@"RestKitLog", @"" __VA_ARGS__)
+RKlcl_log(RKLogComponent, RKlcl_vCritical, @"" __VA_ARGS__);
 
 #define RKLogError(...)                                                                 \
-RKlcl_log(RKLogComponent, RKlcl_vError, @"" __VA_ARGS__);JFLog(@"RestKitLog", @"" __VA_ARGS__)
+RKlcl_log(RKLogComponent, RKlcl_vError, @"" __VA_ARGS__);
 
 #define RKLogWarning(...)                                                               \
-RKlcl_log(RKLogComponent, RKlcl_vWarning, @"" __VA_ARGS__);JFLog(@"RestKitLog", @"" __VA_ARGS__)
+RKlcl_log(RKLogComponent, RKlcl_vWarning, @"" __VA_ARGS__);
 
 #define RKLogInfo(...)                                                                  \
-RKlcl_log(RKLogComponent, RKlcl_vInfo, @"" __VA_ARGS__);JFLog(@"RestKitLog", @"" __VA_ARGS__)
+RKlcl_log(RKLogComponent, RKlcl_vInfo, @"" __VA_ARGS__);
 
 #define RKLogDebug(...)                                                                 \
 RKlcl_log(RKLogComponent, RKlcl_vDebug, @"" __VA_ARGS__)
